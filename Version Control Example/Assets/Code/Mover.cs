85fb3e3c0ec7f1df09b8d245532d38757d786562
Sami Kojo <sami.kojo@gmail.com>
2017-10-18 11:23:11 +0300

    Implemeted Move functionality

--- Version Control Example/Assets/Code/Mover.cs
+++ Version Control Example/Assets/Code/Mover.cs
@@ -1,7 +1,22 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+	public float Speed;
+
+	void Update()
+	{
+		float vertical = Input.GetAxis( "Vertical" );
+		float horizontal = Input.GetAxis( "Horizontal" );
+		
+		Move( new Vector3(horizontal, 0, vertical) );
+	}
+
+	void Move( Vector3 input )
+	{
+		Vector3 movementVector = input * Speed;
+		transform.Translate( movementVector * Time.deltaTime );
+	}
 }
