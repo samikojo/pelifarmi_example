54248a0fa30a12c1f2265a0fa551e1b46003ee6a
Sami Kojo <sami.kojo@gmail.com>
2017-10-18 11:30:37 +0300

    Implemeted Turn feature

--- Version Control Example/Assets/Code/Mover.cs
+++ Version Control Example/Assets/Code/Mover.cs
@@ -4,4 +4,17 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+	public float TurnSpeed = 1;
+
+	void Update()
+	{
+		float turn = Input.GetAxis( "Turn" );
+
+		Turn( turn );
+	}
+
+	void Turn( float amount )
+	{
+		transform.Rotate( Vector3.up * TurnSpeed * Time.deltaTime * amount );
+	}
 }
